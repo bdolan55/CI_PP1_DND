c987f613623a9e59046b73ca6746129f0ae3e975
bdolan55 <brian.e.dolan88@gmail.com>
2022-12-01 20:50:40 +0000

    Comments added to JavaScript file for toggle button

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,3 +1,5 @@
+/* JavaScript Code taken from Web Dev Developed tutorial as linked in README.md */
+
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navbarLinks = document.getElementsByClassName("nav_list")[0]
 
